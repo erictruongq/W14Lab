4080cab70b4dd5b23aa522a7e34a8200cc2a2c47
erictruongq <etruong22@student.cccd.edu>
2024-12-01 00:26:24 -0800

    Hello

---
+++ Lab.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // Include the iostream library for input/output operations
+
+using namespace std; // To avoid using 'std::' before standard functions like cout
+
+int main() {
+    // Print the message to the console
+    cout << "This week's lab is called the W14 - GIT." << endl;
+
+    return 0; // Return 0 to indicate that the program has successfully executed
+}
